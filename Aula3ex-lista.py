eaf4a7a598115928153466e2e557aade9a15337c
Gabriel Lira F. <122597222+lira7011@users.noreply.github.com>
2026-07-29 01:09:22 -0300

    Add files via upload

---
+++ Aula3ex-lista.py
@@ -0,0 +1,12 @@
+numerico = [1,2,3,4,] #lista de números identificados por []
+
+numerico.append(6) #adiciona o número 6 no final
+numerico.remove(2) #remove o 2 da lista
+numerico.remove(4) #remove o 4 da lista(fazer o 4 virar 40, na mesma posição)
+numerico.insert(2, 40) #coloca o 40 na posição do 4 anterior que era 2-
+numerico.sort(reverse=True) #coloca a lista de forma invertida, sort(sortir os números) e 'reverse' como true pra inverter a ordem
+
+print(numerico) #apenas exibe todo e resultado de todas modificações feitas
+
+#obs, listas podem ter listas, dicionarios e outras listas dentro de si
+
