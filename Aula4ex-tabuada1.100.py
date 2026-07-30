6fc9694c6849913f3d5afcc460568bd2a63a3907
Gabriel Lira F. <122597222+lira7011@users.noreply.github.com>
2026-07-30 04:01:40 -0300

    Add files via upload

---
+++ Aula4ex-tabuada1.100.py
@@ -0,0 +1,4 @@
+for n in range(1, 101): #para um numero em um intervalo de 1 a 100:
+    for m in range(1, 11): #e para um outro numero em o intervalo de 1 a 10:
+        resultado = n * m #crie uma variavel resultado que multiplica tanto o n quanto m
+        print(f'{n} x {m} = {resultado}') #exibe a tabuada de 1 a 100 de forma visivelmente melhor com 'x' e '='
