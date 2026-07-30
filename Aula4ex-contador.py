1a1aee228192d29f9a622d1e318e1df50fc27174
Gabriel Lira F. <122597222+lira7011@users.noreply.github.com>
2026-07-30 04:01:06 -0300

    Add files via upload

---
+++ Aula4ex-contador.py
@@ -0,0 +1,4 @@
+contador10 = 1 #admito o valor de um contador se iniciando 1
+while contador10 <= 10: #crio um loop infinito com um adendo que ao chegar em menor/igual a 10 ele chegará ao fim
+    print(contador10) 
+    contador10 += 1 #Pego o contador10 e somo 1 até valor 10
